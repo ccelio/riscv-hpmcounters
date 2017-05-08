c76369aed9f074b8bb8ab9eec6e775efe24a0206
Christopher Celio <celio@eecs.berkeley.edu>
2017-05-07 22:42:09 -0700

    Clean up hpm_counters, remove printfs.

--- hpm_counters.cxx
+++ hpm_counters.cxx
@@ -12,8 +12,12 @@
   asm volatile ("csrr %0, " #reg : "=r"(__tmp)); \
   __tmp; })
 
-#define NUM_COUNTERS (7)
-//#define NUM_COUNTERS (32)
+// 100e6 cycles
+#define SLEEP_TIME_US (100000)
+
+// How many counters do we support? (change for your micro-architecture).
+#define NUM_COUNTERS (8)
+//#define NUM_COUNTERS (32) maximum amount of HPMs is 32
 typedef std::array<long, NUM_COUNTERS> snapshot_t;
 
 static char const*               counter_names[NUM_COUNTERS];
@@ -71,64 +75,62 @@ static int handle_stats(int enable)
          i++; \
       } \
    } while (0)
+
+   // Since most processors will not support all 32 HPMs, comment out which hpm counters you don't want to track.
    READ_CTR(cycle);
    READ_CTR(instret);
+   READ_CTR(time);
    READ_CTR(hpmcounter3);
    READ_CTR(hpmcounter4);
    READ_CTR(hpmcounter5);
-   //READ_CTR(hpmcounter6);
-   //READ_CTR(hpmcounter7);
-   //READ_CTR(hpmcounter8);
-   //READ_CTR(hpmcounter9);
-   //READ_CTR(hpmcounter10);
-   //READ_CTR(hpmcounter11);
-   //READ_CTR(hpmcounter12);
-   //READ_CTR(hpmcounter13);
-   //READ_CTR(hpmcounter14);
-   //READ_CTR(hpmcounter15);
-   //READ_CTR(hpmcounter16);
+   READ_CTR(hpmcounter6);
+   READ_CTR(hpmcounter7);
+   READ_CTR(hpmcounter8);
+   READ_CTR(hpmcounter9);
+   READ_CTR(hpmcounter10);
+   READ_CTR(hpmcounter11);
+   READ_CTR(hpmcounter12);
+   READ_CTR(hpmcounter13);
+   READ_CTR(hpmcounter14);
+   READ_CTR(hpmcounter15);
+   READ_CTR(hpmcounter16);
    READ_CTR(hpmcounter17);
-   //READ_CTR(hpmcounter18);
-   //READ_CTR(hpmcounter19);
-   //READ_CTR(hpmcounter20);
-   //READ_CTR(hpmcounter21);
-   //READ_CTR(hpmcounter22);
-   //READ_CTR(hpmcounter23);
-   //READ_CTR(hpmcounter24);
-   //READ_CTR(hpmcounter25);
-   //READ_CTR(hpmcounter26);
-   //READ_CTR(hpmcounter27);
-   //READ_CTR(hpmcounter28);
-   //READ_CTR(hpmcounter29);
-   //READ_CTR(hpmcounter30);
+   READ_CTR(hpmcounter18);
+   READ_CTR(hpmcounter19);
+   READ_CTR(hpmcounter20);
+   READ_CTR(hpmcounter21);
+   READ_CTR(hpmcounter22);
+   READ_CTR(hpmcounter23);
+   READ_CTR(hpmcounter24);
+   READ_CTR(hpmcounter25);
+   READ_CTR(hpmcounter26);
+   READ_CTR(hpmcounter27);
+   READ_CTR(hpmcounter28);
+   READ_CTR(hpmcounter29);
+   READ_CTR(hpmcounter30);
    READ_CTR(hpmcounter31);
 
    counters.push_back(snapshot);
 
-//   printf("Snapshot Time in cycles : %ld\n", read_csr_safe(cycle) - tsc_start);
-//   printf("Snapshot Time in instret: %ld\n", read_csr_safe(instret) - irt_start);
-   if (step % 10 == 0) printf("heartbeat: %d\n", step);
+   //printf("Snapshot Time in cycles : %ld\n", read_csr_safe(cycle) - tsc_start);
+   //printf("Snapshot Time in instret: %ld\n", read_csr_safe(instret) - irt_start);
+   //if (step % 10 == 0) printf("heartbeat: %d\n", step);
    step++;
 
 #undef READ_CTR
-//   if (enable == FINISH) { 
    if (enable == FINISH || step % 30 == 0) { 
       for (auto & element : counters) {
-//         char buffer [MAX_BUFFER_SZ];
-//         int length = 0;
          for (int i = 0; i < NUM_COUNTERS; i++) {
             long c = element[i];
             if (c) {
-//               length += bytes_added(sprintf(buffer+length, "##  %s = %ld\n", counter_names[i], c));
                printf("##  %s = %ld\n", counter_names[i], c);
             }
          }
-//         printf(buffer);
       }
       if (enable != FINISH) counters.clear();
 
-//      printf("Print Time in cycles : %ld\n", read_csr_safe(cycle) - tsc_start);
-//      printf("Print Time in instret: %ld\n", read_csr_safe(instret) - irt_start);
+      //printf("Print Time in cycles : %ld\n", read_csr_safe(cycle) - tsc_start);
+      //printf("Print Time in instret: %ld\n", read_csr_safe(instret) - irt_start);
    }
 
    if (sigprocmask(SIG_UNBLOCK, &sig_set, NULL) < 0) {
@@ -145,7 +147,7 @@ static int handle_stats(int enable) { return 0; }
 void sig_handler(int signum)
 {
    handle_stats(FINISH);
-   printf("RV Counters exiting, received handler: %d\n", signum);
+   //printf("HPM Counters exiting, received handler: %d\n", signum);
    exit(0);
 }
 
@@ -157,21 +159,20 @@ int main(int argc, char** argv)
    if (argc > 1)
    {
       // only print the final cycle and instret counts.
-      printf ("Pausing, argc=%d\n", argc);
+      //printf ("Pausing, argc=%d\n", argc);
       handle_stats(INIT);
       pause();
    }
    else
    {
-      printf("Starting: counter array size: %d\n", sizeof(counters));
+      //printf("Starting: counter array size: %d\n", sizeof(counters));
       handle_stats(INIT);
       while (1)
       {
-         usleep(100000); // 100e6 cycles
-//         usleep(50000);  // 50M cycles
+         usleep(SLEEP_TIME_US);
          handle_stats(WAKEUP);
       }
-      printf("Exiting\n");
+      //printf("Exiting\n");
    }
 
    return 0;
